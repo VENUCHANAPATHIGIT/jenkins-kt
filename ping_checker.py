385953a0ed4b7afb2cbe8e628366074fdd3103cc
VENUCHANAPATHIGIT <96813823+VENUCHANAPATHIGIT@users.noreply.github.com>
2023-12-09 11:46:11 +0530

    Create ping_checker.py

---
+++ ping_checker.py
@@ -0,0 +1,10 @@
+#Task : Firewall verification(Ping, Traceroute) to 10 servers
+#Stack to use: Python, Jenkins
+
+import subprocess
+
+servers = ['google.com', 'facebook.com']
+
+for i in range(2):
+    ping_result = subprocess.getoutput('ping ' + servers[i])
+    print(ping_result)
